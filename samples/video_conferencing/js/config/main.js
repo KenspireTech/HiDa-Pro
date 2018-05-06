1ad76854b2d880685bcb1abf8ecb8c97ef7d1938
SidDevelop <subha.krish1999@gmail.com>
2018-05-06 20:24:06 +0530

    Update main.js

--- samples/video_conferencing/js/config/main.js
+++ samples/video_conferencing/js/config/main.js
@@ -11,9 +11,9 @@ var QBCONFIG = {
 
 var QBApp = {
     'prod': {
-        'appId': 56252,
-        'authKey': 'yPJfas45qY3jbgq',
-        'authSecret': 'qTFM-eN4vN8rmAw'
+        'appId': 65623,
+        'authKey': '6VqGaNvMXNWdKNt',
+        'authSecret': '5CrVdpqDKCX-GWg'
     }
 };
 
@@ -25,7 +25,7 @@ var QBAppCreds = QBApp.prod;
 
 var MULTIPARTY_SERVER = getQueryVar('server');
 if(!MULTIPARTY_SERVER){
-  var noServerError = "The 'server' query parameter is not set. You need to provide it in order to run the sample. Multi-conference server is available only for Enterprise plans. Please refer to https://quickblox.com/developers/EnterpriseFeatures for more information and contacts."
+  var noServerError = "Click ok"
   alert(noServerError);
   throw noServerError;
 
